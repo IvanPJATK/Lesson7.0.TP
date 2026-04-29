ccb674ee4811d077efa403f4015705a5cc15603d
vertviger <vertviger@gmail.com>
2026-04-29 16:56:43 +0300

    Not working version of GetCustomerReservations

--- TestPrep/TestPrep/Services/RentalService.cs
+++ TestPrep/TestPrep/Services/RentalService.cs
@@ -15,13 +15,58 @@ namespace TestPrep.Services
         public async Task<CustomerRentalDTO?> GetCustomerRentalsAsync(int id)
         {
             await using var connection = new SqlConnection(_connectionString);
-            const string sql =
+            const string customer_sql =
                 """
-                    select rental_id, rental_date, return_date, status_id
-                    from Rental
+                    select first_name, last_name
+                    from Customer
                     where customer_id = @id
                 """;
-            return Ok();
+            await using var command = new SqlCommand(customer_sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            await connection.OpenAsync();
+            await using var reader = await command.ExecuteReaderAsync();
+            var FirstName = reader["first_name"];
+            var LastName = reader["last_name"];
+            //Rentals and movies
+            
+            const string rentalsSql =
+                """
+                    select r.rental_id, r.rental_date, r.return_date, 
+                    s.name as status_name,
+                    m.movie_id, m.title, m.price_per_day
+                    from Rental r
+                    join Status s on r.status_id = s.status_id
+                    join Rental_Item ri on r.rental_id = ri.rental_id
+                    join Movie m on m.movie_id = ri.movie_id
+                    where customer_id = @id
+                """;
+            await using var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            await connection.OpenAsync();
+            await using var readerR = await command.ExecuteReaderAsync();
+            var rentalsAndMovies = new Dictionary<int, RentalsDTO>();
+            while(readerR.ReadAsync())
+            {
+                int rental_id = readerR.GetInt32(readerR.GetOrdinal("rental_id"));
+                if(!rentasAndMovies.TryGetValue(rental_id, out var rental))
+                {
+                    rental = new RentalsDTO
+                    {
+                        Id = rental_id,
+                        RentalDate = reader.GetDateTime(reader.GetOrdinal("rental_date")),
+                        status = reader.GetString(reader.GetOrdinal("status_name")),
+                        movies = new List<MoviesDTO>()
+                    };
+                    rentalsAndMovies.Add(rental_id, rental);
+                }
+                rental.Movies.Add(new MoviesDTO
+                {
+                    MovieId = readerR.GetInt32(readerR.GetOrdinal("movie_id")),
+                    Title = readerR.GetString(readerR.GetOrdinal("title")),
+                    PriceAtRental = readerR.GetDecimal(readerR.GetOrdinal("price_per_day"))
+                });
+            }
+            return rentalsAndMovies.Values;
         }
     }
 }
